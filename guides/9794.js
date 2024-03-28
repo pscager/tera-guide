df3546a17ef7be700949706ade5b8dec367835e9
Dmitry S <dmitry.s@hsdn.org>
2024-03-28 16:25:10 +0400

    Update 9794

--- guides/9794.js
+++ guides/9794.js
@@ -7,7 +7,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		// 1 BOSS
 		"s-794-1000-103-0": [{ type: "text", sub_type: "message", message: "AOE (Dodge)", message_RU: "АОЕ (эвейд)" }],
 		"s-794-1000-107-0": [{ type: "text", sub_type: "message", message: "Jump (Dodge)", message_RU: "Прыжок (эвейд)" }],
-		"s-794-1000-119-0": [{ type: "text", sub_type: "message", message: "Pushback (One Shot)", message_RU: "Откид назад (ваншот)" }],
+		"s-794-1000-119-0": [{ type: "text", sub_type: "message", message: "Pushback", message_RU: "Откид назад" }],
 
 		// 2 BOSS
 		"s-794-2000-112-0": [{ type: "text", sub_type: "message", message: "Bait (3 stones)", message_RU: "Байт (3 камня)" }],
@@ -17,11 +17,11 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-794-2000-114-0": [{ type: "text", sub_type: "message", message: "Together", message_RU: "Собраться" }],
 
 		// 3 BOSS
-		"s-794-3000-103-0": [{ type: "text", sub_type: "message", message: "Pushback (One Shot)", message_RU: "Откид назад (ваншот)" }],
+		"s-794-3000-103-0": [{ type: "text", sub_type: "message", message: "Pushback", message_RU: "Откид назад" }],
 		"s-794-3000-107-0": [{ type: "text", sub_type: "message", message: "Front (Dodge)", message_RU: "Удар вперед (эвейд)" }],
 		"s-794-3000-108-0": [{ type: "text", sub_type: "message", message: "Back (Dodge)", message_RU: "Удар назад (эвейд)" }],
 		"s-794-3000-104-0": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }],
-		"s-794-3000-125-0": [{ type: "text", sub_type: "message", message: "Bait (One Shot)", message_RU: "Байт (ваншот)" }],
+		"s-794-3000-125-0": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }],
 		"s-794-3000-315-0": [{ type: "text", sub_type: "message", message: "Pull", message_RU: "Стяжка" }],
 		"s-794-3000-316-0": "s-794-3000-315-0"
 	};
