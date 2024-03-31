6c8c94e335b8aed23d21659602b9990cb3c04a59
Vampic <satanikon69@gmail.com>
2024-04-01 02:50:02 +0300

    Update 9994

--- guides/9994.js
+++ guides/9994.js
@@ -8,6 +8,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 	let blue_vaccine_loc = null;
 	let red_vaccine_loc = null;
 	let road_from_gameId = null;
+	let temperature_boss = null;
 
 	function spawn_road(loc) {
 		const road_from_ent = entity.mobs[road_from_gameId];
@@ -79,13 +80,14 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-994-2000-114-0": [{ type: "text", sub_type: "message", message: "Together", message_RU: "Собраться" }],
 		"s-994-2000-113-0": [
 			{ type: "text", sub_type: "message", message: "Donut (In > Out)", message_RU: "Бублик (к нему > от него)", delay: 1500 },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, null, 225, 1500, 3000] },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, null, 225, 1500, 2000] },
 			{ type: "text", sub_type: "message", message: "Out", message_RU: "От него", delay: 3500 },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, null, 325, 3500, 3000] }
 		],
 		"s-994-2000-110-0": [{ type: "text", sub_type: "message", message: "Hit in dd", message_RU: "Удар в дд" }],
 
 		// 3 BOSS
+		"ns-994-3000": [{ type: "func", func: () => temperature_boss = null }],
 		"nd-994-3000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
@@ -96,20 +98,41 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"ns-994-3004": [{ type: "func", func: ent => blue_vaccine_loc = ent.pos }], // Синий
 		"ns-994-3005": [{ type: "func", func: ent => red_vaccine_loc = ent.pos }], // Красный
 		"s-994-3000-103-0": [{ type: "text", sub_type: "message", message: "Pushback", message_RU: "Откид назад" }],
-		"s-994-3000-107-0": [{ type: "text", sub_type: "message", message: "Front (Dodge)", message_RU: "Удар вперед (эвейд)" }],
-		"s-994-3000-108-0": [{ type: "text", sub_type: "message", message: "Back (Dodge)", message_RU: "Удар назад (эвейд)" }],
+		"s-994-3000-107-0": [
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 90, 500, 0, 5500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, -90, 500, 0, 5500] },
+			{ type: "text", sub_type: "message", message: "Front (Dodge)", message_RU: "Удар вперед (эвейд)", check_func: () => !temperature_boss },
+			{ type: "text", sub_type: "message", message: "Front | Back (Dodge)", message_RU: "Удар вперед | Удар назад (эвейд)", check_func: () => temperature_boss === "fire" },
+			{ type: "text", sub_type: "message", message: "Front x2 (Dodge)", message_RU: "Удар вперед x2 (эвейд)", check_func: () => temperature_boss === "ice" }
+		],
+		"s-994-3000-108-0": [
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 90, 500, 0, 5500] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, -90, 500, 0, 5500] },
+			{ type: "text", sub_type: "message", message: "Back (Dodge)", message_RU: "Удар назад (эвейд)", check_func: () => !temperature_boss },
+			{ type: "text", sub_type: "message", message: "Back x2 (Dodge)", message_RU: "Удар назад x2 (эвейд)", check_func: () => temperature_boss === "fire" },
+			{ type: "text", sub_type: "message", message: "Back | Front (Dodge)", message_RU: "Удар назад | Удар вперед (эвейд)", check_func: () => temperature_boss === "ice" }
+		],
 		"s-994-3000-104-0": [{ type: "text", sub_type: "message", message: "Bait", message_RU: "Байт" }],
 		"s-994-3000-111-0": [{ type: "text", sub_type: "message", message: "Out Safe", message_RU: "Снаружи сейв" }],
 		"s-994-3000-112-0": [{ type: "text", sub_type: "message", message: "In Safe", message_RU: "Внутри сейв" }],
 		"s-994-3000-113-0": [{ type: "text", sub_type: "message", message: "Donut (In)", message_RU: "Бублик (к нему)" }],
 		"s-994-3000-114-0": [{ type: "text", sub_type: "message", message: "Donut (Out)", message_RU: "Бублик (между)" }],
-		"s-994-3000-116-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
-		"s-994-3000-118-0": [{ type: "text", sub_type: "message", message: "Back", message_RU: "Удар назад" }],
+		"s-994-3000-116-0": [
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 90, 500, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, -90, 500, 0, 3000] },
+			{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед", check_func: () => !temperature_boss }
+		],
+		"s-994-3000-118-0": [
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 90, 500, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, -90, 500, 0, 3000] },
+			{ type: "text", sub_type: "message", message: "Back", message_RU: "Удар назад", check_func: () => !temperature_boss }
+		],
 		"s-994-3000-122-0": [{ type: "text", sub_type: "message", message: "Gather!", message_RU: "Собраться!" }],
 		"s-994-3000-125-0": [{ type: "text", sub_type: "message", message: "Bait (Puddle)", message_RU: "Байт (лужа)" }],
 		"s-994-3000-126-0": "s-994-3000-111-0",
 		"s-994-3000-127-0": "s-994-3000-112-0",
 		"s-994-3000-128-0": "s-994-3000-113-0",
+		"s-994-3000-129-0": "s-994-3000-114-0",
 		"s-994-3000-130-0": "s-994-3000-116-0",
 		"s-994-3000-301-0": [
 			{ type: "text", sub_type: "message", message: "Line up the puddles to blue", message_RU: "Выстроить лужи к синему" },
@@ -121,6 +144,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-994-3000-315-0": [{ type: "text", sub_type: "message", message: "Pull", message_RU: "Стяжка" }],
 		"s-994-3000-316-0": "s-994-3000-315-0",
+		"qb-994-3000-994022": [{ type: "func", func: () => temperature_boss = "ice" }],
+		"qb-994-3000-994024": [{ type: "func", func: () => temperature_boss = "fire" }],
 		"qb-994-3000-994064": [{ type: "text", sub_type: "message", message: "Give stun!", message_RU: "Дать стан!" }],
 		"qb-994-3000-994065": [{ type: "text", sub_type: "message", message: "Gather!", message_RU: "Собраться!" }]
 	};
