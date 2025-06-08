9a8f9a7d6f9b25bc78f9bb5c4a6ad32d3655bb11
Dmitry S <dmitry.s@hsdn.org>
2025-06-09 00:24:08 +0400

    Update 9735

--- guides/9735.js
+++ guides/9735.js
@@ -1,6 +1,6 @@
 ﻿// RK-9 Kennel
 //
-// made by michengs
+// made by michengs / HSDN
 
 module.exports = (dispatch, handlers, guide, lang) => {
 	return {
@@ -9,13 +9,23 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
+		"s-735-1000-104-0": [{ type: "text", sub_type: "message", message: "Front Clip", message_RU: "Передний зажим" }],
+		"s-735-1000-108-0": [{ type: "text", sub_type: "message", message: "Get Out", message_RU: "От него" }], // крутилка
 		"s-735-1000-111-0": [{ type: "text", sub_type: "message", message: "Back + Front", message_RU: "Удар назад + вперед" }],
 		"s-735-1000-112-0": [{ type: "text", sub_type: "message", message: "Back", message_RU: "Удар назад" }],
 		"s-735-1000-304-0": [{ type: "text", sub_type: "message", message: "Out", message_RU: "От него" }],
 		"s-735-1000-305-0": [{ type: "text", sub_type: "message", message: "In", message_RU: "К нему" }],
 		"s-735-1000-306-0": [{ type: "text", sub_type: "message", message: "Bombs", message_RU: "Бомбы" }],
 		"s-735-1000-307-0": [{ type: "text", sub_type: "message", message: "Pull", message_RU: "Стяжка" }],
-		"s-735-1000-309-0": [{ type: "text", sub_type: "message", message: "Four Missile", message_RU: "Запуск 4 ракет" }],
+		"s-735-1000-309-0": [
+			{ type: "text", sub_type: "message", message: "Four Missile", message_RU: "Запуск 4 ракет" },
+			{ type: "text", sub_type: "message", delay: 6000, message: "5", message_RU: "5" },
+			{ type: "text", sub_type: "message", delay: 7000, message: "4", message_RU: "4" },
+			{ type: "text", sub_type: "message", delay: 8000, message: "3", message_RU: "3" },
+			{ type: "text", sub_type: "message", delay: 9000, message: "2", message_RU: "2" },
+			{ type: "text", sub_type: "message", delay: 10000, message: "1", message_RU: "1" },
+			{ type: "text", sub_type: "message", delay: 11000, message: "Jump", message_RU: "Прыгай!" }
+		],
 
 		// 2 BOSS
 		"nd-735-2000": [
@@ -23,7 +33,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "despawn_all" }
 		],
 		"s-735-2000-102-0": [
-			{ type: "text", sub_type: "message", message: "Pizza Cutter", message_RU: "Пила (Эвейд)" },
+			{ type: "text", sub_type: "message", message: "Pizza Cutter", message_RU: "Пила" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 300, 12, 228, 0, 3000] }
 		],
 		"s-735-2000-105-0": [
@@ -39,6 +49,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-735-2000-304-0": [
 			{ type: "text", sub_type: "message", message: "Get Out", message_RU: "От него" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 400, 0, 4000] }
+		],
+		"s-735-2000-305-0": [
+			{ type: "text", sub_type: "message", message: "In | Out", message_RU: "К нему | От него" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 200, 0, 3000] }
 		],
 		"s-735-2007-201-0": [
 			{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 500, 0, 4000] },
@@ -64,7 +78,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
-		"s-735-3000-116-0": [{ type: "text", sub_type: "message", message: "Right Safe", message_RU: "Справа сейф" },
+		"s-735-3000-116-0": [
+			{ type: "text", sub_type: "message", message: "Right Safe", message_RU: "Справа сейф" },
+			{ type: "spawn", func: "marker", args: [false, 160, 300, 0, 900, true, null] },
+			{ type: "spawn", func: "marker", args: [false, 340, 300, 0, 900, true, null] },
 			{ type: "spawn", func: "point", args: [553, 120, 250, 0, 3000] },
 			{ type: "spawn", func: "point", args: [553, 130, 240, 0, 3000] },
 			{ type: "spawn", func: "point", args: [553, 140, 230, 0, 3000] },
@@ -78,7 +95,10 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "point", args: [553, 340, 210, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, 350, 210, 0, 290, 0, 3000] }
 		],
-		"s-735-3000-117-0": [{ type: "text", sub_type: "message", message: "Left Safe", message_RU: "Слева сейф" },
+		"s-735-3000-117-0": [
+			{ type: "text", sub_type: "message", message: "Left Safe", message_RU: "Слева сейф" },
+			{ type: "spawn", func: "marker", args: [false, 20, 300, 0, 900, true, null] },
+			{ type: "spawn", func: "marker", args: [false, 200, 300, 0, 900, true, null] },
 			{ type: "spawn", func: "vector", args: [553, 10, 210, 0, 290, 0, 3000] },
 			{ type: "spawn", func: "point", args: [553, 20, 210, 0, 3000] },
 			{ type: "spawn", func: "point", args: [553, 30, 220, 0, 3000] },
@@ -92,37 +112,39 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "point", args: [553, 200, 210, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, 190, 210, 180, 290, 0, 3000] }
 		],
-		"s-735-3000-118-0": [{ type: "text", sub_type: "message", message: "Left Safe", message_RU: "Слева сейф" },
-			{ type: "spawn", func: "vector", args: [553, 10, 210, 0, 290, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 20, 210, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 30, 220, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 40, 230, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 50, 240, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 60, 250, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 240, 250, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 230, 240, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 220, 230, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 210, 220, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 200, 210, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 190, 210, 180, 290, 0, 3000] }
+		"s-735-3000-118-0": "s-735-3000-117-0",
+		"s-735-3000-119-0": "s-735-3000-116-0",
+		"s-735-3000-125-0": [{ type: "text", sub_type: "message", message: "Front", message_RU: "Удар вперед" }],
+		"s-735-3000-126-0": [{ type: "text", sub_type: "message", message: "Front | Back", message_RU: "Удар вперед | Удар назад" }],
+		"s-735-3000-127-0": [{ type: "text", sub_type: "message", message: "Back", message_RU: "Удар назад" }],
+		"s-735-3000-128-0": [
+			{ type: "text", sub_type: "message", message: "Combo | Back Wave", message_RU: "Комба | Конус назад" },
+			{ type: "spawn", func: "vector", args: [553, 180, 40, 120, 1200, 2000, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 180, 40, 240, 1200, 2000, 3000] }
 		],
-		"s-735-3000-119-0": [{ type: "text", sub_type: "message", message: "Right Safe", message_RU: "Справа сейф" },
-			{ type: "spawn", func: "point", args: [553, 120, 250, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 130, 240, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 140, 230, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 150, 220, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 160, 210, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 170, 210, 180, 290, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 300, 250, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 310, 240, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 320, 230, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 330, 220, 0, 3000] },
-			{ type: "spawn", func: "point", args: [553, 340, 210, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 350, 210, 0, 290, 0, 3000] }
-		],
-		"s-735-3000-128-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Dodge", message_RU: "Эвейд", delay: 1000 }],
+		"s-735-3000-129-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Dodge", message_RU: "Эвейд" }],
 		"s-735-3000-305-0": [{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 300, 0, 7000] }],
 		"s-735-3000-321-0": [{ type: "text", sub_type: "message", message: "Shield!", message_RU: "Щит!" }],
-		"s-735-3000-324-0": [{ type: "text", sub_type: "message", message: "Dodge", message_RU: "Эвейд" }]
+		"s-735-3001-308-0": [
+			{ type: "text", sub_type: "message", message: "Bait!", message_RU: "Байт!" },
+			{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 300, 0, 2000] },
+			{ type: "spawn", func: "vector", args: [912, 0, 0, 90, 300, 0, 2000] },
+			{ type: "spawn", func: "vector", args: [912, 0, 0, 180, 300, 0, 2000] },
+			{ type: "spawn", func: "vector", args: [912, 0, 0, 270, 300, 0, 2000] }
+		],
+		// Radar
+		"qb-735-3000-735312": [{ type: "text", sub_type: "message", message: "!!! Radar !!!", message_RU: "!!! Радар !!!" }],
+		"s-735-3000-324-0": [
+			{ type: "text", sub_type: "message", message: "OUT", message_RU: "ОТ НЕГО" },
+			{ type: "spawn", func: "circle", args: [false, 912, 0, 0, 10, 250, 0, 3000] },
+			{ type: "spawn", func: "circle", args: [false, 912, 0, 0, 12, 200, 0, 3000] },
+			{ type: "spawn", func: "circle", args: [false, 912, 0, 0, 14, 150, 0, 3000] },
+			{ type: "spawn", func: "circle", args: [false, 912, 0, 0, 18, 100, 0, 3000] },
+			{ type: "spawn", func: "circle", args: [false, 912, 0, 0, 50, 50, 0, 3000] }
+		],
+		"s-735-3000-325-0": [
+			{ type: "text", sub_type: "message", message: "IN", message_RU: "К НЕМУ" },
+			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 10, 300, 0, 3000] }
+		]
 	};
 };
